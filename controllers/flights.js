56f799284687d8708a18faeb475c8f5526ddd13b
Mike Mattingly <cmattingly1584@gmail.com>
2021-06-11 00:06:31 -0700

    adds POST method and index view for flights

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,9 +1,29 @@
 const Flight = require('../models/flight');
 
 module.exports = {
-    new: newFlight
+    new: newFlight,
+    create,
+    index
 }
 
 function newFlight(req, res) {
     res.render('flights/new')
+}
+
+function index(req, res) {
+    Flight.find({}, function(err, allFlightDocuments){
+        console.log(allFlightDocuments);
+        res.render('flights/index', {
+            flights: allFlightDocuments
+        })
+    })
+}
+
+function create(req, res) {
+    Flight.create(req.body, function(err, flightDocument) {
+        console.log(err);
+        if (err) return res.render('flights/new');
+        console.log(flightDocument, " < This is the document I created");
+        res.redirect('/flights');
+    })
 }
--- routes/flights.js
+++ routes/flights.js
@@ -1,8 +1,11 @@
 var express = require('express');
+const flights = require('../controllers/flights');
 var router = express.Router();
 const flightsCtrl = require('../controllers/flights');
 
 /* GET users listing. */
 router.get('/new', flightsCtrl.new);
+router.post('/', flightsCtrl.create);
+router.get('/', flightsCtrl.index);
 
 module.exports = router;
